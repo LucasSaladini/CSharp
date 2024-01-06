7bee6fe7190a212cdd7524e10f782f4bf47bcfe2
Lucas Saladini <lucassaladini@hotmail.com>
2024-01-06 18:58:04 -0300

    Utilizando operadores condicionais e switch case

---
+++ Operadores_Condicionais/Switch_Case/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Digite uma letra");
+string letra = Console.ReadLine();
+
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal");
+        break;
+    default:
+        Console.WriteLine("Não é uma vogal");
+        break;
+}

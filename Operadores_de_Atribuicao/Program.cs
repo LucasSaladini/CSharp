550c18593d915a99c2f2413c39590a68b1fec345
Lucas Saladini <lucassaladini@hotmail.com>
2024-01-06 18:31:59 -0300

    Operadores de atribuição

---
+++ Operadores_de_Atribuicao/Program.cs
@@ -0,0 +1,30 @@
+﻿// int a = 10;
+// int b = 20;
+
+// int c = a + b;
+
+// c += 5;
+// c -= 5;
+
+// Console.WriteLine(c);
+
+
+// Casting
+// int a = Convert.ToInt32("5");
+// int a = int.Parse("5");
+// int inteiro = 5;
+// string a = inteiro.ToString();
+
+// int a = 5;
+// double b = a;
+
+// Console.WriteLine(b);
+
+string a = "15-";
+
+int b = 0;
+
+int.TryParse(a, out b);
+
+Console.WriteLine(b);
+Console.WriteLine("Conversão realizada com sucesso");

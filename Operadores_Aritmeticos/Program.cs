b67ee82ce34bfe57458812cdb7626c9f9a19cedc
Lucas Saladini <lucassaladini@hotmail.com>
2024-01-07 11:44:03 -0300

    Working with mathematical operators

---
+++ Operadores_Aritmeticos/Program.cs
@@ -0,0 +1,23 @@
+﻿using Operadores_Aritmeticos.Models;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 30);
+calc.Multiplicar(10, 30);
+calc.Subtrair(10, 30);
+calc.Dividir(10, 30);
+calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Cosseno(30);
+calc.Tangente(30);
+
+int numero = 10;
+
+Console.WriteLine($"Incrementando o {numero}");
+numero++;
+Console.WriteLine($"{numero}");
+Console.WriteLine($"Decrementanto o {numero}");
+numero--;
+Console.WriteLine($"{numero}");
+
+calc.RaizQuadrada(9);

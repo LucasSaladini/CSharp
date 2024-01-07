1067e351ed158a99a45852cf047ca326023ec9ef
Lucas Saladini <lucassaladini@hotmail.com>
2024-01-07 11:01:44 -0300

    Working with logical operators

---
+++ Operadores_Logicos/Program.cs
@@ -0,0 +1,35 @@
+﻿// bool eMaiorDeIdade = false;
+// bool possuiAutorizacaoDoResponsavel = false;
+
+// if (eMaiorDeIdade || possuiAutorizacaoDoResponsavel)
+// {
+//     Console.WriteLine("Entrada liberada!");
+// }
+// else
+// {
+//     Console.WriteLine("Entrada não liberada");
+// }
+
+// bool possuiFrequenciaMinima = true;
+// double media = 6.5;
+
+// if (possuiFrequenciaMinima && media >= 7)
+// {
+//     Console.WriteLine("Aprovado");
+// }
+// else
+// {
+//     Console.WriteLine("Reprovado");
+// }
+
+bool choveu = false;
+bool estaTarde = false;
+
+if (!choveu && !estaTarde)
+{
+    Console.WriteLine("Vou pedalar");
+}
+else
+{
+    Console.WriteLine("Vou pedalar outro dia");
+}

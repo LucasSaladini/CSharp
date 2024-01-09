ff21071f1c14a82bc22abf6db6793c4f33f44f05
Lucas Saladini <lucassaladini@hotmail.com>
2024-01-09 19:20:00 -0300

    Working with project and references

---
+++ Estrutura_de_um_Projeto/ExemploFundamentos.Common/Models/Calculadora.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace ExemploFundamentos.Common.Models
+{
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} +  {y} = {x + y}");
+        }
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} x {y} = {x * y}");
+        }
+        public void Dividir(double x, double y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+        public void Potencia(int x, int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x} elevado a {y} = {pot}");
+        }
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"O seno de {angulo}° = {Math.Round(seno, 4)}");
+        }
+        public void Cosseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double cosseno = Math.Cos(radiano);
+            Console.WriteLine($"O cosseno de {angulo}° = {Math.Round(cosseno, 4)}");
+        }
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"A tangente de {angulo}° = {Math.Round(tangente, 4)}");
+        }
+        public void RaizQuadrada(double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"A raiz quadrada de {x} = {raiz}");
+        }
+    }
+}
---
+++ Estrutura_de_um_Projeto/ExemploFundamentos/Program.cs
@@ -0,0 +1,6 @@
+﻿using ExemploFundamentos.Common.Models;
+
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Lucas";
+pessoa1.Idade = 30;
+pessoa1.Apresentar();

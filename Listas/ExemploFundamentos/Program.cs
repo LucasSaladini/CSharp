6bf619de7efc0dbaef796d0da4555b6af8518fc7
Lucas Saladini <lucassaladini@hotmail.com>
2024-01-10 19:11:36 -0300

    Working with lists

---
+++ Listas/ExemploFundamentos/Program.cs
@@ -0,0 +1,33 @@
+﻿using ExemploFundamentos.Common.Models;
+
+List<string> listaString = new List<string>();
+
+listaString.Add("SP");
+listaString.Add("BA");
+listaString.Add("MG");
+listaString.Add("RJ");
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+listaString.Add("SC");
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+listaString.Remove("MG");
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+Console.WriteLine("Lista com for");
+for (int contador = 0; contador < listaString.Count; contador++)
+{
+    Console.WriteLine($"Posição N° {contador} - {listaString[contador]}");
+}
+
+Console.WriteLine("Lista com foreach");
+int contadorForeach = 0;
+
+foreach (string item in listaString)
+{
+    Console.WriteLine($"Posição N° {contadorForeach} - {item}");
+    contadorForeach++;
+}

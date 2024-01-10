4a456087ae07cabcb821ccb498b1d3a6dab61b22
Lucas Saladini <lucassaladini@hotmail.com>
2024-01-10 18:57:31 -0300

    Working with arrays

---
+++ Array/ExemploFundamentos/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExemploFundamentos.Common.Models;
+
+int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+
+int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
+// Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+// for (int contador = 0; contador < arrayInteiros.Length; contador++)
+// {
+//     Console.WriteLine($"Na posição {contador} o valor é {arrayInteiros[contador]}");
+// }
+
+int contadorForeach = 0;
+
+foreach (int valor in arrayInteirosDobrado)
+{
+    Console.WriteLine($"Posição N° {contadorForeach} - {valor}");
+    contadorForeach++;
+}
+

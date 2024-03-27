5eedaec54dbe5880b210197e3b86db9d90f44d27
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-03-27 15:52:18 -0300

    Working with constructors

---
+++ Constructores/ExemploConstrutores/Program.cs
@@ -0,0 +1,14 @@
+﻿using ExemploConstrutores.Models;
+
+Pessoa p1 = new Pessoa("Lucas", "Saladini");
+p1.Apresentar();
+
+Log log = Log.GetInstance();
+
+log.PropriedadeLog = "Teste instância";
+
+Log log2 = Log.GetInstance();
+System.Console.WriteLine(log2.PropriedadeLog); 
+
+Aluno a1 = new Aluno("Lucas", "Saladini", "Teste");
+a1.Apresentar();

c7a573e63db54a356b5822cbe013c75874ead68a
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-03-27 13:48:51 -0300

    Instanciated Pessoa class

--- Ref_E_Struct/Program.cs
+++ Ref_E_Struct/Program.cs
@@ -1,2 +1,15 @@
 using static System.Console;
 
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Lucas";
+p1.Idade = 30;
+p1.EnderecoPessoa = new Endereco ()
+{
+  Logradouro = "Rua Teste",
+  Cidade = "São Paulo",
+  CEP = "00000000",
+  Numero = 300
+};
+
+WriteLine("Fim");

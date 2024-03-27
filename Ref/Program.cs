0de12b4c551ad67af79aa57b2aa690c8c7b66b4a
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-03-27 13:40:57 -0300

    Update Program.cs

--- Ref/Program.cs
+++ Ref/Program.cs
@@ -2,29 +2,7 @@ using static System.Console;
 
 class Ref
 {
-  static void Demo1()
-  {
-    int a  = 5;
-    
-    Adicionar20(ref a);
-    
-    WriteLine($"O valor de a é {a}");
-  }
-  static void Adicionar20(ref int a)
-  {
-    a += 20;
-  }
-  static void AlterarNome(string[] nomes, string nome, string nomeNovo)
-  {
-    for(int i = 0; i < nomes.Length; i++)
-    {
-      if(nomes[i] == nome)
-      {
-        nomes[i] = nomeNovo;
-      }
-    }
-  }
-  static void Main()
+  static void Demo2()
   {
     var nomes = new string[] {"José", "Maria", "Ricardo", "Alice", "Pedro"};
     WriteLine("Digite o nome que deseja que seja substituído");
@@ -37,5 +15,58 @@ class Ref
     WriteLine($@"A lista de nomes Alterada é:
       {string.Join(", \n", nomes)}
     ");
+  }
+  static void Demo1()
+  {
+    int a  = 5;
+    
+    Adicionar20(ref a);
+    
+    WriteLine($"O valor de a é {a}");
+  }
+  static void Adicionar20(ref int a)
+  {
+    a += 20;
+  }
+  static ref string AlterarNome(string[] nomes, string nome, string nomeNovo)
+  {
+    for(int i = 0; i < nomes.Length; i++)
+    {
+      if(nomes[i] == nome)
+      {
+        nomes[i] = nomeNovo;
+      }
+    }
+  }
+  static int LocalizarNome(string[] nomes, string nome)
+  {
+    for(int i = 0; i < nomes.Length; i++)
+    {
+      if(nomes[i] == nome)
+        return ref nomes[i];
+    }
+    throw new Exception("Nome não encontrado");
+  }
+  static void Main()
+  {
+    var nomes = new string[] {"José", "Maria", "Ricardo", "Alice", "Pedro"};
+    WriteLine("Digite o nome que deseja que seja substituído");
+    var nome = ReadLine();
+    WriteLine("Digite o nome novo:");
+    var nomeNovo = ReadLine();
+
+    ref var nomeAchado = ref LocalizarNome(nomes, nome);
+
+    if(!string.IsNullOrWhiteSpace(nomeAchado))
+    {
+      nomes[indice] = nomeNovo;
+      WriteLine($@"A lista de nomes Alterada é:
+        {string.Join(", \n", nomes)}
+      ");
+    }
+    else
+    {
+      WriteLine("Nome não encontrado");
+    }
   }
 }

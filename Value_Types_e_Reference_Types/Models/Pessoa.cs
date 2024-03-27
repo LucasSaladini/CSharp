2a7e4411eadaa0eb7d939d45661f71e474faf5db
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-03-27 09:43:09 -0300

    Update Pessoa.cs with clone method

--- Value_Types_e_Reference_Types/Models/Pessoa.cs
+++ Value_Types_e_Reference_Types/Models/Pessoa.cs
@@ -11,4 +11,14 @@ namespace Value_Types_e_Reference_Types.Models
         public string Nome { get; set; }
         public string Documento { get; set; }
     }
+
+    public Pessoa Clone()
+    {
+        return new Pessoa()
+        {
+            Documento = this.Documento,
+            Idade = this.Idade,
+            Nome = this.Nome
+        }
+    }
 }

5487470c778b35b95bc1181b7db76c95e997ffec
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-03-27 11:53:37 -0300

    Created a method to replace old name

--- Ref/Program.cs
+++ Ref/Program.cs
@@ -2,16 +2,40 @@ using static System.Console;
 
 class Ref
 {
-  static void Adicionar20(ref int a)
-  {
-    a += 20;
-  }
-  static void Main()
+  static void Demo1()
   {
     int a  = 5;
     
     Adicionar20(ref a);
     
     WriteLine($"O valor de a é {a}");
+  }
+  static void Adicionar20(ref int a)
+  {
+    a += 20;
+  }
+  static void AlterarNome(string[] nomes, string nome, string nomeNovo)
+  {
+    for(int i = 0; i < nomes.Length; i++)
+    {
+      if(nomes[i] == nome)
+      {
+        nomes[i] = nomeNovo;
+      }
+    }
+  }
+  static void Main()
+  {
+    var nomes = new string[] {"José", "Maria", "Ricardo", "Alice", "Pedro"};
+    WriteLine("Digite o nome que deseja que seja substituído");
+    var nome = ReadLine();
+    WriteLine("Digite o nome novo:");
+    var nomeNovo = ReadLine();
+
+    AlterarNome(nomes, nome, nomeNovo);
+
+    WriteLine($@"A lista de nomes Alterada é:
+      {string.Join(", \n", nomes)}
+    ");
   }
 }

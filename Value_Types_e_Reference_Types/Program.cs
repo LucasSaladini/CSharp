56d6dc84ddb1b3ddefd49e5248f6a425b60b7f4b
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-03-27 11:17:14 -0300

    Using PessoaStruct method

--- Value_Types_e_Reference_Types/Program.cs
+++ Value_Types_e_Reference_Types/Program.cs
@@ -133,9 +133,46 @@ public class Program
 		}
 		return -1;
 	}
-	
+		static bool EncontrarPessoa(List<Pessoa> pessoas, Pessoa pessoa)
+		{
+			foreach(var item in pessoas)
+			{
+				if(item.Equals(pessoa))
+					return true;
+			}
+			return false;
+		}
+		static bool EncontrarPessoa(List<StructPessoa> pessoas, StructPessoa pessoa)
+			{
+				foreach(var item in pessoas)
+				{
+					if(item == pessoa)
+						return true;
+				}
+				return false;
+			}
     public static void Main()
   {
-			
+			List<StructPessoa> pessoas = new List<StructPessoa>()
+			{
+				new StructPessoa(){ Nome = "Ricardo" },
+				new StructPessoa(){ Nome = "José" },
+				new StructPessoa(){ Nome = "Maria" },
+				new StructPessoa(){ Nome = "Fabiana" },
+				new StructPessoa(){ Nome = "Eduardo" }
+			};
+
+			WriteLine($"Digite a pessoa que gostaria de localizar:");
+			var nome = ReadLine();
+			var pessoa = new StructPessoa(){ Nome = nome };
+			var encontrado = EncontrarPessoa(pessoas, pessoa)
+			if(encontrado)
+			{
+				WriteLine($"Pessoa localizada");
+			}
+			else
+			{
+				WriteLine($"Pessoa não localizada");
+			}
   }
 }

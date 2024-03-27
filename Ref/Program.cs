41c61de4c2e4065b669636c86aa837a1b249aa17
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-03-27 11:33:20 -0300

    Using ref on a method

---
+++ Ref/Program.cs
@@ -0,0 +1,17 @@
+using static System.Console;
+
+class Ref
+{
+  static void Adicionar20(ref int a)
+  {
+    a += 20;
+  }
+  static void Main()
+  {
+    int a  = 5;
+    
+    Adicionar20(ref a);
+    
+    WriteLine($"O valor de a é {a}");
+  }
+}

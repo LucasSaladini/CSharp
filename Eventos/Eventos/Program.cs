818887858fdd0481b2bb5772272060d76644c74a
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-03-28 08:53:38 -0300

    Working with events

---
+++ Eventos/Eventos/Program.cs
@@ -0,0 +1,13 @@
+﻿using Eventos.Models;
+
+namespace Eventos
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Matematica m = new Matematica(10, 20);
+            m.Somar();
+        }
+    }
+}

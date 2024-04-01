a25c5e1438803a60e5a4290149819ebcffdb2d18
Lucas Saladini <lucassaladini@hotmail.com>
2024-04-01 19:20:50 -0300

    Printing a multi dimensional array using for loop

--- Colecoes_e_LINQ/Array/Colecoes/Program.cs
+++ Colecoes_e_LINQ/Array/Colecoes/Program.cs
@@ -17,3 +17,23 @@ foreach (var item in arrayInteiros)
 {
     Console.WriteLine(item);
 }
+
+// Array Multidimensional
+    // array com 4 linhas e 2 colunas
+int[,] matriz = new int[4,2]
+{
+    { 8, 8 },
+    { 10, 20 },
+    { 50, 100 },
+    { 90, 200 }
+};
+
+Console.WriteLine("Percorrendo o array multidimensional");
+
+for (int i = 0; i < matriz.GetLength(0); i++)
+{
+    for (int j = 0; j < matriz.GetLength(1); j++)
+    {
+        Console.WriteLine(matriz[i, j]);
+    }
+}
--- Colecoes_e_LINQ/Array/Colecoes/obj/Debug/net8.0/Colecoes.AssemblyInfo.cs
+++ Colecoes_e_LINQ/Array/Colecoes/obj/Debug/net8.0/Colecoes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Colecoes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d36efb97c8d3aa93de270b0f79967b8a5f2258cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a0d55e24db76aba73883792a86eac1a7b41fe5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Colecoes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Colecoes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

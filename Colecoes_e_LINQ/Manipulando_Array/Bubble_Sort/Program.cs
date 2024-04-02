b3612e10f2e5255e43812ad51651ae8053a54fc8
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-04-02 14:00:01 -0300

    Sortin array using bubble sort

---
+++ Colecoes_e_LINQ/Manipulando_Array/Bubble_Sort/Program.cs
@@ -0,0 +1,12 @@
+﻿using Bubble_Sort.Helper;
+
+OperacoesArray op = new OperacoesArray();
+int[] array = new int[5] { 6, 3, 8, 1, 9 };
+
+Console.WriteLine("Array Original");
+op.ImprimirArray(array);
+
+op.OrdernarBubbleSort(ref array);
+
+Console.WriteLine("Array Ordenado");
+op.ImprimirArray(array);

b93a67b36f58e37bc93aa9604e69690c259a5d60
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-04-03 09:35:14 -0300

    Adding and printing list items

---
+++ Colecoes_e_LINQ/Colecoes_Genericas/Colecoes_Genericas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+List<string> estados = new List<string>();
+
+estados.Add("SP");
+estados.Add("MG");
+estados.Add("BA");
+
+System.Console.WriteLine($"Quantidade de elementos na lista: {estados.Count}");
+
+System.Console.WriteLine("--------------");
+System.Console.WriteLine("Imprimindo através do foreach");
+
+foreach (var item in estados)
+{
+    System.Console.WriteLine(item);
+}
+
+System.Console.WriteLine("--------------");
+System.Console.WriteLine("Imprimindo através do for");
+
+for (int i = 0; i < estados.Count; i++)
+{
+    System.Console.WriteLine($"Índice {i}: {estados[i]}");
+}

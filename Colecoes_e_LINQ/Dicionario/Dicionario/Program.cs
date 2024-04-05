295f46c5a75cca9b1659d3f4aec78f0240be717f
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-04-05 17:13:24 -0300

    Created Dictionary

---
+++ Colecoes_e_LINQ/Dicionario/Dicionario/Program.cs
@@ -0,0 +1,10 @@
+﻿Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("MG", "Minas Gerais");
+estados.Add("BA", "Bahia");
+
+foreach (var item in estados)
+{
+    System.Console.WriteLine($"Chave: {item.Key} - Valor: {item.Value}");
+}

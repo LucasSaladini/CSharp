c7626d10257021638b22dd2a5e881dd295e446d1
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-04-05 17:03:39 -0300

    Working with Stack

--- Colecoes_e_LINQ/Colecoes_Especificas/Colecoes_Especificas/Program.cs
+++ Colecoes_e_LINQ/Colecoes_Especificas/Colecoes_Especificas/Program.cs
@@ -13,3 +13,20 @@ while (fila.Count > 0)
     System.Console.WriteLine($"Pessoas na fila: {fila.Count}");
 
 }
+
+System.Console.WriteLine("Pilha");
+
+Stack<string> pilhaLivros = new Stack<string>();
+
+pilhaLivros.Push(".NET");
+pilhaLivros.Push("DDD");
+pilhaLivros.Push("Código Limpo");
+
+System.Console.WriteLine($"Livros para leitura: {pilhaLivros.Count}");
+
+while(pilhaLivros.Count > 0)
+{
+    System.Console.WriteLine($"Próximo livro para a leitura: {pilhaLivros.Peek()}");
+    System.Console.WriteLine($"{pilhaLivros.Pop()} lido com sucesso");
+    System.Console.WriteLine($"Livros na fila: {pilhaLivros.Count}");
+}

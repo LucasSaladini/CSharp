e1f0e673dbd353021dfd7e91dd45c5712f077a57
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-04-05 16:52:52 -0300

    Working with Queue

---
+++ Colecoes_e_LINQ/Colecoes_Especificas/Colecoes_Especificas/Program.cs
@@ -0,0 +1,15 @@
+﻿Queue<string> fila = new Queue<string>();
+
+fila.Enqueue("Lucas");
+fila.Enqueue("Eduardo");
+fila.Enqueue("André");
+
+System.Console.WriteLine($"Pessoas na fila: {fila.Count}");
+
+while (fila.Count > 0)
+{
+    System.Console.WriteLine($"Vez de: {fila.Peek()}");
+    System.Console.WriteLine($"{fila.Dequeue()} atendido");
+    System.Console.WriteLine($"Pessoas na fila: {fila.Count}");
+
+}

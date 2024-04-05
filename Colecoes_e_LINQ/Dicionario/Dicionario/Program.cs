aef949fdf531e078dca59284a4d8aa2d4e9188cb
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-04-05 17:21:13 -0300

    Removing a value from Dictionary

--- Colecoes_e_LINQ/Dicionario/Dicionario/Program.cs
+++ Colecoes_e_LINQ/Dicionario/Dicionario/Program.cs
@@ -21,3 +21,14 @@ System.Console.WriteLine("Atuaizando um valor do Dictionary");
 estados[valorProcurado] = "BA - Atualizado";
 
 System.Console.WriteLine(estados[valorProcurado]);
+
+System.Console.WriteLine("--------");
+System.Console.WriteLine("Removendo um valor do Dictionary");
+System.Console.WriteLine($"Removendo o valor: {estados[valorProcurado]}");
+
+estados.Remove(valorProcurado);
+
+foreach (var item in estados)
+{
+    System.Console.WriteLine($"Chave: {item.Key} - Valor: {item.Value}");
+}
